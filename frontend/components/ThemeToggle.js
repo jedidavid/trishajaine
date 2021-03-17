b2d59f0e0b1733da3e37011da21bf84adfb0bf00
Jeremy David <jeremydb.david@gmail.com>
2021-03-17 13:01:22 +0400

    dark and light mode

--- frontend/components/ThemeToggle.js
+++ frontend/components/ThemeToggle.js
@@ -9,12 +9,12 @@ const ThemeToggle = () => {
       {theme === "dark" ? (
         <HiSun
           onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-          className="text-gray-500 dark:text-gray-400 text-2xl cursor-pointer"
+          className="text-black dark:text-white text-2xl cursor-pointer"
         />
       ) : (
         <HiMoon
           onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-          className="text-gray-500 dark:text-gray-400 text-2xl cursor-pointer"
+          className="text-black dark:text-black text-2xl cursor-pointer"
         />
       )}
     </div>
