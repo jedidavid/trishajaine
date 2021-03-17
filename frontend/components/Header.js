dba082bb39c8db29228282ef05b40693d9d21456
Jeremy David <jeremydb.david@gmail.com>
2021-03-17 13:00:41 +0400

    header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,11 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
-    <div>
-      <ThemeToggle />
-    </div>
+    <>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg mb-3">
+        <div className="container mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a
+              className="text-6xl font-heading leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
+              href="#"
+            >
+              TJ
+            </a>
+            <button
+              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto items-center">
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
+                  href="#"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
+                  href="#"
+                >
+                  Projects
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
+                  href="#"
+                >
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center leading-snug"
+                  href="#"
+                >
+                  <ThemeToggle />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
