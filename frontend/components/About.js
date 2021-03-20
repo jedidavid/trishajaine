b251d61458ae9a80397ffc877296a2d3ab0a2be8
Jedi David <jedidavid@Jedis-iMac-Pro.local>
2021-03-20 23:14:42 +0400

    about section

---
+++ frontend/components/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container py-32">
+      <h2 className="text-5xl">
+        <span className="relative pb-2 title-decorator">About</span>
+      </h2>
+      <p className="text-lgp py-12">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
+        adipisci magnam sunt. Voluptatum maiores temporibus pariatur dolorem
+        quaerat adipisci commodi culpa molestias amet hic totam illum tenetur,
+        minus asperiores eos. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Facere vero quasi pariatur, exercitationem maxime debitis tempore
+        consequuntur accusantium. Architecto illo animi aliquid veritatis, eius
+        rerum exercitationem consequuntur repudiandae vitae omnis!
+      </p>
+    </div>
+  );
+};
+
+export default About;
