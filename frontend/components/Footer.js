549dc929331a492757e472c3a5a6defafae43501
Jedi David <jeremydb.david@gmail.com>
2021-03-20 23:17:49 +0400

    replace email

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -10,8 +10,10 @@ const Footer = () => {
               <p className="text-white dark:text-black text-sm pb-2">
                 Don't be shy. Say hi! &#128075;
               </p>
-              <h1 className="text-8xl text-white dark:text-black border-b-2 border-white dark:border-black base-transition text-transition border-transition">
-                <a href="#">tj@email.com</a>
+              <h1 className="text-7xl text-white dark:text-black border-b-2 border-white dark:border-black base-transition text-transition border-transition">
+                <a href="mailto:trishadavidb@gmail.com">
+                  trishadavidb@gmail.com
+                </a>
               </h1>
             </div>
           </div>
