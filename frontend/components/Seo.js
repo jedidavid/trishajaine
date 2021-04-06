a5dffc5ac69360204c051b7766585319ab9fb4b0
Jedi David <jeremydb.david@gmail.com>
2021-04-06 22:47:23 +0400

    SEO

--- frontend/components/Seo.js
+++ frontend/components/Seo.js
@@ -5,30 +5,14 @@ const Seo = (props) => {
   return (
     <>
       <NextSeo
-        title={`${props.title} | ${process.env.NEXT_PUBLIC_PROJECT_NAME}`}
-        description="This example uses more of the available config options."
-        canonical="https://www.canonical.ie/"
+        title={`Trisha Jaine | ${props.title}`}
+        description="My architecture portfolio"
+        canonical="https://www.trishajaine.com/"
         openGraph={{
           url: props.link,
-          title: props.title,
+          title: `Trisha Jaine | ${props.title}`,
           description: props.desc,
-          images: [
-            {
-              url: "https://www.example.ie/og-image-01.jpg",
-              width: 800,
-              height: 600,
-              alt: "Og Image Alt",
-            },
-            {
-              url: "https://www.example.ie/og-image-02.jpg",
-              width: 900,
-              height: 800,
-              alt: "Og Image Alt Second",
-            },
-            { url: "https://www.example.ie/og-image-03.jpg" },
-            { url: "https://www.example.ie/og-image-04.jpg" },
-          ],
-          site_name: "Website Name",
+          site_name: "Trisha Jaine",
         }}
       />
     </>
