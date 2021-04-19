2f07ea5a9efb482a9f30a657353260d289aa9ce1
Jedi David <jeremydb.david@gmail.com>
2021-04-19 20:40:11 +0400

    fix cv props

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 const Layout = ({ children, global }) => {
   return (
     <div className="page">
-      <Header cv={global.cv.url} />
+      <Header cv={global.cv} />
       <main>{children}</main>
       <Footer
         email={global.email}
