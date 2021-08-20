ec99ad43dfe3254187442a0a615e92b680e404b3
Jedi David <jeremydb.david@gmail.com>
2021-08-20 10:05:44 +0400

    update config to accomodate digitalocean

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,6 +1,3 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
@@ -8,17 +5,14 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       options: {
-        ssl: true,
+        ssl: false,
       },
     },
   },
